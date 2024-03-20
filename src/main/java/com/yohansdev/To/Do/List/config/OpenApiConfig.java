81900430d973159add33f9cce6b0cdc926f1b1f2
Yohans Nascimento <yohans.dev@gmail.com>
2024-03-20 19:37:25 -0300

    docs: added swagger ui for documentation

---
+++ src/main/java/com/yohansdev/To/Do/List/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.yohansdev.To.Do.List.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "YohansDev",
+                        email = "yohans.dev@gmail.com",
+                        url = "https://yohans-dev.netlify.app"
+                ),
+                description = "API para gerenciamento de tarefas.",
+                title = "To Do List - NUVEM",
+                version = "1.0"
+        ))
+public class OpenApiConfig {
+}
--- src/main/java/com/yohansdev/To/Do/List/controllers/TaskController.java
+++ src/main/java/com/yohansdev/To/Do/List/controllers/TaskController.java
@@ -5,11 +5,11 @@ import com.yohansdev.To.Do.List.models.dtos.CreateTaskDto;
 import com.yohansdev.To.Do.List.models.dtos.TaskResponseDto;
 import com.yohansdev.To.Do.List.models.dtos.UpdateTaskDto;
 import com.yohansdev.To.Do.List.services.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,30 +17,34 @@ import java.util.List;
 
 @Validated
 @RestController
-@RequestMapping("/task")
+@RequestMapping("todo/task")
 public class TaskController {
     @Autowired
     TaskService taskService;
 
     @GetMapping
+    @Operation(summary = "Lista todas as tarefas cadastradas.", method = "GET")
     public ResponseEntity getAllTasks(){
         List<Task> tasks = taskService.getAllTasks();
         return ResponseEntity.ok(tasks);
     }
 
     @PostMapping
+    @Operation(summary = "Realiza o cadastro de uma nova tarefa.", method = "POST")
     public ResponseEntity createTask(@Valid @RequestBody CreateTaskDto createTaskDto){
         TaskResponseDto taskCreated = taskService.createTask(createTaskDto);
         return ResponseEntity.ok(taskCreated);
     }
 
     @PutMapping
+    @Operation(summary = "Atualiza as tarefas previamente cadastradas.", method = "PUT")
     public ResponseEntity updateTask(@Valid @RequestBody UpdateTaskDto updateTaskDto){
         Task task = taskService.updateTask(updateTaskDto);
         return ResponseEntity.ok(task);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Apagar uma tarefa.", method = "DELETE")
     public ResponseEntity deleteTask(@PathVariable @NotNull(message = "O ID não pode ser nulo.")
                                          @Positive(message = "O ID deve ser positivo.") Long id){
         taskService.deleteTask(id);
